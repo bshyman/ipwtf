e26098f03fe89ae77f1f7dcf5a918f700b0c9b56
bshy <bshyman@gmail.com>
2019-04-13 19:32:26 -0500

    clean up ui

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -3,7 +3,7 @@ class Interface < ApplicationRecord
   attr_accessor :hostname
 
   validates_format_of :ip, with: /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/,
-                      message:   ' is invalid'
+                      message:   ' needs to be in valid IP format'
   validates_format_of :ddns, with: /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/,
                       message:     'address is invalid', multiline: true, allow_blank: true
 end
