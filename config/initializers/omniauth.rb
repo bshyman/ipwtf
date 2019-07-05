629825ac0a073c21a98827b75b47e33bab12cc7c
bshy <bshyman@gmail.com>
2019-07-04 22:14:22 -0500

    secrets -> credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret
+  provider :google_oauth2, Rails.application.credentials.google_client_id, Rails.application.credentials.google_client_secret
 end
